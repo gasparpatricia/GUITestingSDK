334e0b8eae3ec443f664604f9d2dd25a2fbb4be7
Patri <ptrc.gaspar@gmail.com>
2020-06-08 23:13:41 +0300

    Test commit v2

--- TestObjects/TestObjectRequisites/TestObjectBase.cs
+++ TestObjects/TestObjectRequisites/TestObjectBase.cs
@@ -171,6 +171,8 @@ namespace GUITestingSDK
             if (type == null)
                 throw new InvalidOperationException("Object type not found.");
             // hello world
+
+            //hello world v2
             return Activator.CreateInstance(type, testObject);
         }
         
